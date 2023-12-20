9edf7ffe8b11095215270796df275d6e5562ceb5
Arifulislam5577 <arifalamm33@gmail.com>
2023-12-20 13:27:17 +0600

    Home Page Updated.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ const App = () => {
     <main className="min-h-screen grid place-items-center bg-slate-900">
       <div>
         <h1 className="text-center max-w-sm mx-auto text-5xl font-bold text-white">
-          Learn Github Action from Youtube
+          Learn Github Action from Youtube.
         </h1>
       </div>
     </main>
