e4fd903cb51330c91713b19e761b8842fe9cb2db
Arifulislam5577 <arifalamm33@gmail.com>
2023-12-20 13:46:53 +0600

    Added: Animation added in the homepage.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <main className="min-h-screen grid place-items-center bg-slate-900">
       <div>
-        <h1 className="text-center max-w-sm mx-auto text-5xl font-bold text-white">
+        <h1 className="text-center max-w-sm mx-auto text-5xl font-bold text-white border p-5 rounded animate-pulse">
           Learn Github Action from Youtube.
         </h1>
       </div>
